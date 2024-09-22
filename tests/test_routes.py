b155acb6c5ac728288381070941d1369f873681a
McMcGrey <arkohlba@gmail.com>
2024-09-22 14:17:40 -0400

    added errorhandler test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -199,3 +199,8 @@ class TestAccountService(TestCase):
             content_type="application/json"
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
